4bc7636ad3c45bffe07e4a5c6c884a2bf2f21342
diemvi2724 <duongthidiemvi2004@gmail.com>
2026-08-24 05:20:45 +0700

    Update PatientRepository.java

--- PatientRepository.java
+++ PatientRepository.java
@@ -1,13 +1,24 @@
 package com.project.back_end.repo;
 
-import com.project.back_end.models.User;
+import com.project.back_end.models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface PatientRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String email);
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    Optional<Patient> findByEmail(String email);
+
+    Optional<Patient> findByPhone(String phone);
+
+    @Query("SELECT p FROM Patient p WHERE LOWER(p.firstName) LIKE LOWER(CONCAT('%', :name, '%')) " +
+           "OR LOWER(p.lastName) LIKE LOWER(CONCAT('%', :name, '%'))")
+    List<Patient> searchByName(@Param("name") String name);
+
     boolean existsByEmail(String email);
 }
