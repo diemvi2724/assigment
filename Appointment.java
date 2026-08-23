b726256c4111678f1c4402a21c5f0b5e1515fe57
diemvi2724 <duongthidiemvi2004@gmail.com>
2026-08-24 01:00:09 +0700

    Create Appointment.java

---
+++ Appointment.java
@@ -0,0 +1,72 @@
+package com.project.back_end.models;
+
+import java.time.LocalDateTime;
+
+public class Appointment {
+    private int appointmentId;
+    private int patientId;
+    private int doctorId;
+    private LocalDateTime appointmentDate;
+    private String status;
+    private LocalDateTime createdAt;
+
+    public Appointment() {
+    }
+
+    public Appointment(int appointmentId, int patientId, int doctorId, LocalDateTime appointmentDate, String status, LocalDateTime createdAt) {
+        this.appointmentId = appointmentId;
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.appointmentDate = appointmentDate;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public LocalDateTime getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDateTime appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
